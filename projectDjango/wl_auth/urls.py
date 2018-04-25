fdd94127a30dfaa76c4466c3370b2793f8026f61
wararom <ibbwrr.26091996@gmail.com>
2018-04-25 16:46:41 +0700

    new

---
+++ projectDjango/wl_auth/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path,include
+
+urlpatterns = [
+	path('signin/', views.signin, name='signin'),
+	path('signout/', views.signout, name='signout'),
+	path('change_password/', views.change_password, name='change_password'),
+]
